68d9f77e24fab556833badda22ecaad3e9e80ccb
tangjingwen <tjw_codefarmer@163.com>
2018-09-26 23:08:15 +0800

    firset

---
+++ src/main/java/com/tjw/tchat/TchatApplication.java
@@ -0,0 +1,12 @@
+package com.tjw.tchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TchatApplication.class, args);
+    }
+}
