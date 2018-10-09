d50e9b82ae44da3b805373bdb1a7d98b881eaeeb
tangjingwen <tjw_codefarmer@163.com>
2018-10-09 21:09:10 +0800

    add websocket end

---
+++ src/main/java/com/tjw/tchat/websocket/ChatHandler.java
@@ -0,0 +1,53 @@
+package com.tjw.tchat.websocket;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Mr.Tang
+ * @date 2018/10/9
+ * 处理消息的handler
+ * textwebSocketFrame:在netty中，是用于websocket专门处理文本的对象，frame是消息的载体
+ */
+public class ChatHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+    // 用于记录和管理所有客户端的channel
+    private static ChannelGroup clients = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg)
+            throws Exception {
+        // 获取客户端传递过来的内同
+        String content = msg.text();
+        System.out.println("接收到的数据:"+content);
+        for (Channel channel:clients){
+            // 需要用textwebsocketframe作为载体
+            channel.writeAndFlush(new TextWebSocketFrame("[服务器在]"+LocalDateTime.now()
+                    +"，接收到消息，消息为:"+content));
+        }
+        // 也可以直接使用cliens的writeandflush方法
+//        clients.writeAndFlush(new TextWebSocketFrame("[服务器在]"+LocalDateTime.now()
+//                +"，接收到消息，消息为:"+content));
+    }
+
+    @Override
+    /**
+     * 打开客户端时，将channel放到group里
+     */
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        clients.add(ctx.channel());
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        // 当出发handlerRemoved方法，clients会自动移除
+//        clients.remove(ctx.channel());
+        System.out.println("客户端断开，对应的长ID："+ctx.channel().id().asLongText());
+        System.out.println("客户端断开，对应的短ID："+ctx.channel().id().asShortText());
+    }
+}
---
+++ src/main/java/com/tjw/tchat/websocket/WSServerInitializer.java
@@ -0,0 +1,38 @@
+package com.tjw.tchat.websocket;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * @author Mr.Tang
+ * @date 2018/10/8
+ */
+public class WSServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        ChannelPipeline channelPipeline = socketChannel.pipeline();
+        // websocket基于http，所以需要http的解编码器
+        channelPipeline.addLast(new HttpServerCodec());
+        // 对写达数据流的支持
+        channelPipeline.addLast(new ChunkedWriteHandler());
+        // 对httpmessage进行聚合，聚合fullhttprequest和fullhttpresponse
+        // 几乎在所有的netty编程中都会使用此handler
+        channelPipeline.addLast(new HttpObjectAggregator(1024*64));
+        // =======================以上是支持http协议
+
+        /**
+         * websocket 服务器处理的协议，用于指定给客户端访问的路由地址:ws
+         * 本handler会帮助你处理一些繁杂的事情
+         * 会处理握手动作:handshaking(close,ping,pong) ping+pong=心跳
+         * 对于websocket，都是以frames进行传输的，不同数据类型对应的frames也不同
+         */
+        channelPipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
+        // 自定义handler
+        channelPipeline.addLast(new ChatHandler());
+    }
+}
